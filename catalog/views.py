b1dde2b06da8d0ac2de38c6ead0c65490512a5d3
konstantin <kgranevitch@gmail.com>
2025-09-24 01:09:33 +0700

    add readme, bootstrap-static & functional in catalog app

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_view(request):
+    """функция-контроллер для вывода домашней страницы приложения каталог"""
+
+    if request.method == "GET":
+        return render(request, 'catalog/home.html')
+
+def contacts_view(request):
+    """функция-контроллер для вывода страницы с контактами приложения каталог"""
+
+    if request.method == "GET":
+        return render(request, 'catalog/contacts.html')
