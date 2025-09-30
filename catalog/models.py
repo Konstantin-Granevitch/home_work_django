e2d6da47afa937971b19dfe342bd3c46341713f6
konstantin <kgranevitch@gmail.com>
2025-10-01 02:11:12 +0700

    add fixture & castomize function

--- catalog/models.py
+++ catalog/models.py
@@ -8,13 +8,13 @@ class Category(models.Model):
     )
 
     def __str__(self):
-        return f'категория {self.name}'
+        return f"категория {self.name}"
 
     class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
-        ordering = ['name']
-        db_table = 'Категории'
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
+        db_table = "Категории"
 
 
 class Product(models.Model):
@@ -23,7 +23,7 @@ class Product(models.Model):
         null=True, blank=True, verbose_name="Описание", help_text="Введите описание продукта"
     )
     image = models.ImageField(upload_to="images/", verbose_name="Изображение")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='Products')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="Products")
     purchase_price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Цена за покупку")
     created_at = models.DateField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateField(auto_now=True, verbose_name="Дата последнего изменения")
